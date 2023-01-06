524e456f8f49f47cf0b877921aa157c954831dc7
romzyyy <fijay0117@gmail.com>
2023-01-06 18:32:14 +0700

    conflig

--- app/src/main/java/com/example/finalproject/admin.java
+++ app/src/main/java/com/example/finalproject/admin.java
@@ -30,7 +30,6 @@ public class admin extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
