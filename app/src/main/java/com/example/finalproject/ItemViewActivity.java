7b42dd9f25d8eaf730161dc0ae43b0576cc7b08a
romzyyy <fijay0117@gmail.com>
2023-01-06 16:13:43 +0700

    add item in itemView

--- app/src/main/java/com/example/finalproject/ItemViewActivity.java
+++ app/src/main/java/com/example/finalproject/ItemViewActivity.java
@@ -2,28 +2,32 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class ItemViewActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView textViewName, textViewDesc;
 
     ItemsModel itemsModel;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_view);
 
-        textView = findViewById(R.id.textViewName);
+        textViewName = findViewById(R.id.textViewName);
+        textViewDesc = findViewById(R.id.textViewDesc);
 
         Intent intent = getIntent();
 
         if(intent.getExtras() != null){
             itemsModel = (ItemsModel) intent.getSerializableExtra("item");
-            textView.setText(itemsModel.getName());
+            textViewName.setText(itemsModel.getName());
+            textViewDesc.setText(itemsModel.getDesc());
         }
     }
 }
